132d0656c335d090a20348b81397000c9a093c22
karunakar <karunakar_yalla@apple.com>
2020-05-16 14:19:24 -0700

    Adding Core module and Home, Topnav components

---
+++ SrMarket/src/app/core/core.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TopnavComponent } from './topnav/topnav.component';
+
+@NgModule({
+  declarations: [TopnavComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule { }
