f96af71a8f56840747944fab9be8269ff48ef4c3
karunakar <karunakar_yalla@apple.com>
2020-08-05 20:02:21 -0700

    Added Saved Content and Profile pages

--- SrMarket/src/app/app-routing.module.ts
+++ SrMarket/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './home/login/login.component';
 import { ProductsComponent } from './home/products/products.component';
 import { DealsComponent } from './home/deals/deals.component';
+import { AboutComponent } from './home/about/about.component';
 
 const routes: Routes = [
   {
@@ -15,7 +16,8 @@ const routes: Routes = [
     component : HomeComponent,
     children : [
       {path: 'app-products', component: ProductsComponent },
-      {path: 'app-deals' , component: DealsComponent}
+      {path: 'app-deals' , component: DealsComponent},
+      {path: 'app-about', component: AboutComponent}
     ]
   },
 
--- SrMarket/src/app/app.module.ts
+++ SrMarket/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { DealsComponent } from './home/deals/deals.component';
 import { ParentComponent } from './home/parent/parent.component';
 import { ChildComponent } from './home/child/child.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { AboutComponent } from './home/about/about.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
     ProductsComponent,
     DealsComponent,
     ParentComponent,
-    ChildComponent
+    ChildComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
